6a2e25699cc571fc480126a6dbe1c2c6bb3a7a12
C. Davis <connor0095@gmail.com>
2016-01-16 14:42:47 -0600

    Update main.js

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -1,7 +1,7 @@
 import { extend } from 'flarum/extend';
 import app from 'flarum/app';
 
-app.initializers.add('flarum-ext-logo', () => {
+app.initializers.add('santiagobiali-logo', () => {
     document.getElementById("home-link").innerHTML = 
             "<img src='"+app.forum.attribute('logo_url')+"'>";
 },-100);
